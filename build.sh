d2745c9b81309c311eb7fb19dba76a6c0c4109c4
huangbinbin <huangbinbin@deepvision-tech.com>
2024-03-17 05:44:36 +0000

    feat - arm64 build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 set -x
 
 currentpath=$(pwd)
-installpath=$currentpath/../vcpkg_installed/x64-linux
+installpath=$currentpath/../vcpkg_installed/arm64-linux
+#installpath=$currentpath/../vcpkg_installed/x64-linux
 mkdir -p $installpath
 
 paths=("$currentpath/prism/build" "$currentpath/prism_container/build" "$currentpath/prism_qt_modular/build" "$currentpath/prism_qt_core/build" "$currentpath/prism_qt_ui/build")
@@ -10,7 +11,8 @@ paths=("$currentpath/prism/build" "$currentpath/prism_container/build" "$current
 for path in ${paths[@]}; do
    mkdir -p $path
    cd $path
-   cmake .. -DCMAKE_PREFIX_PATH="$installpath;$QT_DIR" -DCMAKE_INSTALL_PREFIX="$installpath"
+   cmake .. -DCMAKE_INSTALL_PREFIX=$installpath -DCMAKE_TOOLCHAIN_FILE=/repos/vcpkg/toolchains/arm64-raspberrypie.cmake
+   #cmake .. -DCMAKE_PREFIX_PATH="$installpath;$QT_DIR" -DCMAKE_INSTALL_PREFIX="$installpath" -DCMAKE_PREFIX_PATH="$installpath;$QT_DIR" -DCMAKE_INSTALL_PREFIX="$installpath"
    if [ $? -ne 0 ]; then
     exit 1
    fi
