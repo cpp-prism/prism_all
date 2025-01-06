4700705ad8e91f0aef507e779614f2c2c8406532
hbb_mac <kok.bing@qq.com>
2025-01-06 13:11:20 +0800

    fix - vncitem export
    | Submodule prism_qt_ui 7a366e4..795f666:
    |   > fix - vncitem export

--- clear.sh
+++ clear.sh
@@ -2,8 +2,6 @@
 set -x
 
 currentpath=$(pwd)
-installpath=$currentpath/../vcpkg_installed/x64-linux
-mkdir -p $installpath
 
 paths=("$currentpath/prism/build" "$currentpath/prism_container/build" "$currentpath/prism_qt_modular/build" "$currentpath/prism_qt_core/build" "$currentpath/prism_qt_ui/build")
 
