3900654744eb5d867b1ec4e03b335cc633d84757
huangbinbin <kok.bing@qq.com>
2025-10-19 22:15:17 +0800

    feat - qt6 first commit

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -x
 
 currentpath=$(pwd)
 
-paths=("$currentpath/prism/build" "$currentpath/prism_container/build" "$currentpath/prism_qt_modular/build" "$currentpath/prism_qt_core/build" "$currentpath/prism_qt_ui/build")
+paths=("$currentpath/prism/build" "$currentpath/prism_container/build" "$currentpath/prism_qt_modular/build" "$currentpath/prism_qt_core/build" )
 
 for path in ${paths[@]}; do
    mkdir -p $path
